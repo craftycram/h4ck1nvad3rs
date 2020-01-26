d50ecde81c81e8825afdb50fb24ba8a5d4caeced
craftycram <github@craftycram.net>
2020-01-26 13:46:52 +0100

    refactored bullets in bullet class

---
+++ Bullet.js
@@ -0,0 +1,9 @@
+// eslint-disable-next-line no-unused-vars
+class Bullet {
+  constructor(posX, posY, id) {
+    // @Flo: Zwischen optional und required Parametern unterscheiden?! zb rand
+    this.posX = posX;
+    this.posY = posY;
+    this.id = id;
+  }
+}

befc853c7a839b566cda8e4d540c4f8b3e85d59c
craftycram <github@craftycram.net>
2020-02-07 21:05:21 +0100

    added comments

--- Invader.js
+++ Invader.js
@@ -1,16 +1,16 @@
 // eslint-disable-next-line no-unused-vars
 class Invader {
   constructor(width, height, posX, posY, id, rand = Math.random()) {
-    this.width = width;
-    this.height = height;
-    this.posX = posX;
-    this.posY = posY;
-    this.rand = rand;
-    this.appearance = this.generateInvader();
-    this.id = id;
-    this.explodeState = 0;
-    this.lasers = [];
-    this.laserId = 0;
+    this.width = width; // invaders width
+    this.height = height; // invaders height
+    this.posX = posX; // invader x position
+    this.posY = posY; // invader y position
+    this.rand = rand; // invader appearance random number
+    this.appearance = this.generateInvader(); // saved appearance
+    this.id = id; // invaders id
+    this.explodeState = 0; // invaders explosion state
+    this.lasers = []; // current invader lasers
+    this.laserId = 0; // laser id
   }
 
   shootLaser() {
@@ -18,6 +18,7 @@ class Invader {
     console.log(`Invader ${this.id} Laser shot`);
     // eslint-disable-next-line no-undef
     this.lasers.push(new Laser(this.posX + this.width,
+      // creates new instance of Laser class and pushes into lasers
       this.posY + this.height, this.laserId += 1));
   }
 
@@ -25,16 +26,18 @@ class Invader {
     // eslint-disable-next-line no-console
     console.log(`Invader ${this.id} exploded`);
 
-    if (this.explodeState < 1) {
+    if (this.explodeState < 1) { // check if hasn't already exploded
+    // cycles through x coordinates of invader's appearance
       for (let x = 0; x < this.width; x += 1) {
+        // cycles through y coordinates of invader's appearance
         for (let y = 0; y < this.height; y += 1) {
           // const randProcessed = Math.random().toString()[x * y];
           // if (parseInt(randProcessed, 10) > 5) {
-          if (Math.random() > 0.5) {
-            if (Math.random() > 0.5) {
-              this.appearance[x][y] = 2;
+          if (Math.random() > 0.5) { // randomly decide to replace pixel or not
+            if (Math.random() > 0.5) { // randomly decide between empty / exlosion pixel
+              this.appearance[x][y] = 2; // set explosion pixel
             } else {
-              this.appearance[x][y] = 0;
+              this.appearance[x][y] = 0; // remove pixel
             }
           }
         }
@@ -60,18 +63,20 @@ class Invader {
   generateInvader() {
     const invader = [];
     // const rand = Math.random();
+    // cycles through x coordinates of invader's appearance
     for (let x = 0; x < this.width; x += 1) {
+      // cycles through y coordinates of invader's appearance
       for (let y = 0; y < this.height; y += 1) {
-        const randProcessed = this.rand.toString()[x * y];
-        if (parseInt(randProcessed, 10) > 3) {
-          if (!Array.isArray(invader[x])) {
-            invader[x] = [];
+        const randProcessed = this.rand.toString()[x * y]; // proecss random value
+        if (parseInt(randProcessed, 10) > 3) { // create pixel if random is greater than 3
+          if (!Array.isArray(invader[x])) { // check if array exists at pos x
+            invader[x] = []; // create array at pos x
             invader[x][y] = 1;
           } else {
             invader[x][y] = 1;
           }
-        } else if (!Array.isArray(invader[x])) {
-          invader[x] = [];
+        } else if (!Array.isArray(invader[x])) { // check if array exists at pos x
+          invader[x] = []; // create array at pos x
           invader[x][y] = 0;
         } else {
           invader[x][y] = 0;
--- InvaderRow.js
+++ InvaderRow.js
@@ -2,26 +2,29 @@
 class InvaderRow {
   constructor(posX, posY, cols, invaderCount, id, invWidth, invHeight, rand) {
     // @Flo: Zwischen optional und required Parametern unterscheiden?! zb rand
-    this.posX = posX;
-    this.posY = posY;
-    this.cols = cols;
-    this.rand = rand;
-    this.invWidth = invWidth;
-    this.invHeight = invHeight;
-    this.invaderCount = invaderCount;
-    this.id = id;
+    this.posX = posX; // invader row x position
+    this.posY = posY; // invader row y position
+    this.cols = cols; // screen width
+    this.rand = rand; // random value for invaders appearance
+    this.invWidth = invWidth; // invader width
+    this.invHeight = invHeight; // invader height
+    this.invaderCount = invaderCount; // number of invaders in row
+    this.id = id; // row id
+    // spaces / number of empty pixels between invaders
     this.spaces = this.cols / this.invaderCount / 2;
+    // width of all invaders + spaces
     this.rowWidth = this.invWidth * this.invaderCount + this.spaces * this.invaderCount;
-    this.invaders = this.generateInvaderRow();
+    this.invaders = this.generateInvaderRow(); // all invaders
   }
 
-  generateInvaderRow() {
+  generateInvaderRow() { // creates invaders
     const temp = [];
-    let xPos = this.spaces + this.posX;
+    let xPos = this.spaces + this.posX; // invader x pos
     for (let i = 0; i < this.invaderCount; i += 1) {
+      // creates instance of Invader class
       // eslint-disable-next-line no-undef
       temp.push(new Invader(this.invWidth, this.invHeight, xPos, this.posY, i, this.rand));
-      xPos += this.spaces + this.invWidth;
+      xPos += this.spaces + this.invWidth; // increase invader x position
     }
     return temp;
   }
